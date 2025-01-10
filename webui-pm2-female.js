dabb31911a30876d67a6b539bfc11876cb585e00
Bui Dac Huy <huybuidac@cyberk.io>
2025-01-10 09:52:38 +0700

    Update webui-pm2-female.js

--- webui-pm2-female.js
+++ webui-pm2-female.js
@@ -1,8 +1,24 @@
 const {exec} = require('child_process')
 
+let p
 const main = () => {
-    const p = exec('webui-user-female.bat')
+    p = exec('webui-user-female.bat')
     p.stdout.on('data', m => console.log(m))
 }
+
+// Ensure the child process is killed when the parent exits
+process.on('exit', () => {
+  console.log('Parent process exiting. Killing child process...');
+  p.kill();
+});
+
+// Handle termination signals
+['SIGINT', 'SIGTERM'].forEach(signal => {
+  process.on(signal, () => {
+    console.log(`Received ${signal}, terminating...`);
+    p.kill();
+    process.exit();
+  });
+});
 
 main()
